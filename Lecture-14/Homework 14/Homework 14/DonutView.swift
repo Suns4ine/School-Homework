9ec6da775b5f550aa29cc07b16dfd96c85e88392
Vyacheslav <pronin.slavok@mail.ru>
2021-07-23 23:14:54 +0300

    что-то есть

---
+++ Lecture-14/Homework 14/Homework 14/DonutView.swift
@@ -0,0 +1,8 @@
+//
+//  DonutView.swift
+//  Homework 14
+//
+//  Created by Vyacheslav Pronin on 23.07.2021.
+//
+
+import Foundation
---
+++ Lecture-14/Homework 14/Homework 14/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  Homework 14
+//
+//  Created by Vyacheslav Pronin on 23.07.2021.
+//
+
+import Foundation
