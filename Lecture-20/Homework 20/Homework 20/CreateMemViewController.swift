91b9255dbf3b5c49894a85fea7ddb6719d040ed5
Vyacheslav <pronin.slavok@mail.ru>
2021-08-06 20:29:51 +0300

    Сделал добавление и сохранение фотографии

--- Lecture-20/Homework 20/Homework 20/CreateMemViewController.swift
+++ Lecture-20/Homework 20/Homework 20/CreateMemViewController.swift
@@ -7,18 +7,45 @@
 
 import Foundation
 import UIKit
-import PhotosUI
 
-final class CreateMemViewController: UIViewController {
+final class CreateMemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
  
-    private let saveItem: UIBarButtonItem = UIBarButtonItem(title: "Сохранить",
+
+    private var imageViewHeightConstraint: NSLayoutConstraint?
+    private var imageViewWidthConstraint: NSLayoutConstraint?
+    private let screenSize = UIScreen.main.bounds
+    
+    private lazy var saveItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "save"),
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(saveMeme))
-    private let addImage: UIBarButtonItem = UIBarButtonItem(title: "Добавить картинку",
+    private lazy var addImageItem: UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "image"),
                                                             style: .plain,
                                                             target: self,
-                                                            action: #selector(saveMeme))
+                                                            action: #selector(addImage))
+    
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.textColor = .textColor
+        textView.backgroundColor = .clear
+        textView.font = UIFont(name: "Al Nile", size: 24)
+        textView.showsVerticalScrollIndicator = false
+        textView.text = "Вбейте текст"
+        textView.sizeToFit()
+        textView.textContainer.maximumNumberOfLines = 3
+        return textView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .clear
+        imageView.layer.borderColor = UIColor.textColor.cgColor
+        imageView.layer.borderWidth = 6
+        return imageView
+    }()
     
     private let settingView: UIView = {
         let view = UIView()
@@ -33,14 +60,88 @@ final class CreateMemViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.rightBarButtonItems = [ saveItem, addImage]
+        self.navigationItem.rightBarButtonItems = [saveItem, addImageItem]
+        [settingView, imageView, textView].forEach{ view.addSubview($0)}
         
-        [settingView].forEach{ view.addSubview($0)}
+        imageViewHeightConstraint = imageView.heightAnchor.constraint(equalToConstant: 0)
+        imageViewWidthConstraint = imageView.widthAnchor.constraint(equalToConstant: 0)
+        imageViewHeightConstraint?.isActive = true
+        imageViewWidthConstraint?.isActive = true
     }
     
     @objc
     private func saveMeme() {
+        guard let image = imageView.image else { return }
+
+           UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
         
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    @objc
+    private func addImage() {
+        
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in self.openCamera() }))
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in self.openGallery() }))
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func openGallery(){
+       if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+           let imagePicker = UIImagePickerController()
+           imagePicker.delegate = self
+           //imagePicker.allowsEditing = true
+           imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+           self.present(imagePicker, animated: true, completion: nil)
+       } else {
+           let alert  = UIAlertController(title: "Warning", message: "You don't have permission to access gallery.", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+           self.present(alert, animated: true, completion: nil)
+       }
+   }
+    
+    private func openCamera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    internal func imagePickerController(_ picker: UIImagePickerController,
+                                        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageViewHeightConstraint?.constant =  pickedImage.size.height > screenSize.height/2.2 ?
+                screenSize.height/2.2 : pickedImage.size.height
+            imageViewWidthConstraint?.constant =  pickedImage.size.width > screenSize.width - 40 ?
+                screenSize.width - 40 : pickedImage.size.width
+            imageView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLayoutSubviews() {
@@ -50,13 +151,50 @@ final class CreateMemViewController: UIViewController {
             settingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             settingView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             settingView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            settingView.heightAnchor.constraint(equalToConstant: 100)
+            settingView.heightAnchor.constraint(equalToConstant: 80),
+            
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            textView.heightAnchor.constraint(equalToConstant: 100),
+
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
-}
+    
+//    func keyboardWillShow(sender: NSNotification) {
+//        self.view.frame.origin.y -= 150
+//    }
+//    func keyboardWillHide(sender: NSNotification) {
+//        self.view.frame.origin.y += 150
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-extension CreateMemViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        dismiss(animated: true)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillHide() {
+        self.view.frame.origin.y = 0
+    }
+
+    @objc func keyboardWillChange(notification: NSNotification) {
+
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if textView.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
